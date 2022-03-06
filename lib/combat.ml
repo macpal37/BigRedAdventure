61eca1a09f13bb37a6b302aaa39310be40e1ca0a
esicilia731 <esicilia731@gmail.com>
2022-03-06 17:49:30 -0500

    Implemented combat ml and mli

--- lib/combat.ml
+++ lib/combat.ml
@@ -1,1 +1,79 @@
- 
+open Creature
+
+type bstatus =
+  | Victory
+  | Loss
+  | Flee
+  | Catch
+  | Ongoing
+
+type btype =
+  | Trainer
+  | Wild
+
+type battle_record = {
+  player_creatures : creature list;
+  enemy_creatures : creature list;
+  battle_type : btype;
+  battle_status : bstatus;
+  escape_attempts : int;
+}
+
+exception Empty
+
+let reset_clist_player cpair brecord =
+  match brecord.player_creatures with
+  | _ :: t -> fst cpair :: t
+  | _ -> raise Empty
+
+let reset_clist_enemy cpair brecord =
+  match brecord.enemy_creatures with
+  | _ :: t -> snd cpair :: t
+  | _ -> raise Empty
+
+let execute_move player_or_enemy attack_id brecord =
+  if player_or_enemy then
+    let out_pair =
+      Move.execute_move attack_id
+        (List.nth brecord.player_creatures 0)
+        (List.nth brecord.enemy_creatures 0)
+    in
+
+    {
+      brecord with
+      player_creatures = reset_clist_player out_pair brecord;
+      enemy_creatures = reset_clist_enemy out_pair brecord;
+    }
+  else
+    let out_pair =
+      Move.execute_move attack_id
+        (List.nth brecord.enemy_creatures 0)
+        (List.nth brecord.player_creatures 0)
+    in
+
+    {
+      brecord with
+      player_creatures = reset_clist_player out_pair brecord;
+      enemy_creatures = reset_clist_enemy out_pair brecord;
+    }
+
+let run_away brecord =
+  let pspeed =
+    List.nth (get_stats (List.nth brecord.player_creatures 0)) 6
+  in
+  let espeed =
+    List.nth (get_stats (List.nth brecord.enemy_creatures 0)) 6
+  in
+  let odds_escape =
+    ((pspeed * 32 / (espeed / 4 mod 256)) + 30)
+    * brecord.escape_attempts
+  in
+  if
+    pspeed >= espeed || odds_escape > 255
+    || Random.int 256 > odds_escape
+  then { brecord with battle_status = Flee }
+  else { brecord with escape_attempts = brecord.escape_attempts + 1 }
+
+(*let capture brecord = let e_currHP = List.nth (get_stats (List.nth
+  brecord.enemy_creatures 0)) 1 let e_maxHP = List.nth (get_stats
+  (List.nth brecord.enemy_creatures 0)) 0 let e_rate = *)
--- lib/combat.mli
+++ lib/combat.mli
@@ -1,1 +1,25 @@
-(** Parsing of player commands. *)
+open Creature
+
+(*Parsing of player commands. *)
+type battle_record
+(** The abstract type that represents the standing data of a Pokemon
+    battle at a given turn. This type will store the pokemon engaged in
+    battle, as well as their evolving victory status.*)
+
+(*val get_moves : creature -> string list (**Given a creature,
+  [get_moves] returns a string of all moves it current knows.*)*)
+
+val execute_move : creature -> string -> battle_record
+(**Given a creature, as well as a string containing the move ID,
+   [execute_move] produces an updated battle record.*)
+
+(*val rand_move : creature -> string (**Given a creature, a move is
+  randomly chosen from its list of acceptable moves*)*)
+
+val run_away : battle_record -> battle_record
+(**Given a battle record, checks if the player is able to run away. If
+   so, return battle_record with victory status set as Flee.*)
+
+(*val capture : battle_record -> battle_record (**Given a battle record,
+  checks if the player is able to catch the creature. If so, return
+  battle_record with victory status set as Catch*)*)
