bd5e52c7836e77c627364f296401d61303143b65
Alan Chen <aac77@cornell.edu>
2022-03-06 12:12:20 -0500

    player.mli written

--- lib/player.mli
+++ lib/player.mli
@@ -1,1 +1,32 @@
-(** Parsing of player commands. *)
+(** Representation of core player attributes
+
+    This module represents the attributes of the player, such as their
+    name, or which badges they have explored*)
+
+type player
+(** The abstract type that represents a player*)
+
+val new_player : string -> player
+(** [new_player s] is a player with name [s], no money, no time played,
+    no badges*)
+
+val name : player -> string
+(** [name p] is the name of player [p]*)
+
+val money : player -> int
+(** [money p] is the money held by player [p]*)
+
+val time_played : player -> int
+(** [time_played p] is the time played by player [p]*)
+
+val badges : player -> string list
+(** [badges p] are the badges held by player [p]*)
+
+val add_money : int -> player -> player
+(** [add_money i p] is player [p] with [i] more money*)
+
+val add_time_played : int -> player -> player
+(** [add_time_played i p] is player [p] with [i] more time played*)
+
+val add_badge : string -> player -> player
+(** [add_badge b p] is player [p] with added badeg [b]*)
