8ad0e17c47652ee6426b9a426fae691895061f13
Alan Chen <aac77@cornell.edu>
2022-03-11 18:22:32 -0500

    Implemented inventory

--- lib/inventory.ml
+++ lib/inventory.ml
@@ -1,2 +1,51 @@
+open Item
 
- 
+exception Insufficient of int
+
+module ItemTypeMap = Stdlib.Map.Make (struct
+  type t = Item.item_type
+
+  let compare = compare
+end)
+
+module ItemMap = Stdlib.Map.Make (struct
+  type t = Item.item
+
+  let compare = compare
+end)
+
+type bag = int ItemMap.t ref
+type inventory = bag ItemTypeMap.t ref
+
+let new_inventory _ = ref ItemTypeMap.empty
+
+let get_bag (i : inventory) (t : Item.item_type) =
+  match ItemTypeMap.find_opt t !i with
+  | Some b -> b
+  | None ->
+      let b = ref ItemMap.empty in
+      i := ItemTypeMap.add t b !i;
+      b
+
+let list_items b = ItemMap.to_seq !b
+
+let add b i ?(c = 1) =
+  b :=
+    ItemMap.update i
+      (fun k ->
+        match k with
+        | Some v -> Some (v + c)
+        | None -> Some c)
+      !b
+
+let consume b i ?(c = 1) =
+  b :=
+    ItemMap.update i
+      (fun k ->
+        match k with
+        | Some v ->
+            let new_count = v - c in
+            if new_count >= 0 then Some new_count
+            else raise (Insufficient v)
+        | None -> raise (Insufficient 0))
+      !b
--- lib/inventory.mli
+++ lib/inventory.mli
@@ -1,1 +1,37 @@
-(** Parsing of player commands. *)
+(** Representation of an inventory
+
+    This module represents a player inventory, divided into categories
+    that track the number of copies the player has of items*)
+
+open Item
+
+exception Insufficient of int
+(** Raised when attempting to consume more copies of an item than
+    present. The associated integer is the actual number of copies
+    present*)
+
+type inventory
+(** The abstract type that represents the inventory*)
+
+type bag
+(** The abstract type that represents a collection of items of a certain
+    type*)
+
+val new_inventory : unit -> inventory
+(** [new_inventory] is an empty inventory*)
+
+val get_bag : inventory -> item_type -> bag
+(** [get_bag i t] is the item bag in inventory [i] that stores item of
+    type [t]*)
+
+val list_items : bag -> (item * int) list
+(** [list_items b] is list of pairs of items and their number of copies
+    present in [b]*)
+
+val add : bag -> item -> ?count:int -> unit
+(** [add b i c] adds [c] copies of item [t] in bag [b]. [c] is 1 by
+    default. This call mutates [b]*)
+
+val consume : bag -> item -> ?count:int -> unit
+(** [consume b i c] removes [c] copies of item [t] from bag [b]. [c] is
+    1 by default. This call mutates [b]*)
