a416787583db032de7e73be5e04f0d60fa13ba2e
Alan Chen <aac77@cornell.edu>
2022-03-14 11:02:56 -0400

    Minor fix to Inventory.list_items

--- lib/inventory.ml
+++ lib/inventory.ml
@@ -43,7 +43,8 @@ let consume b ?(count = 1) i =
         match k with
         | Some v ->
             let new_count = v - count in
-            if new_count >= 0 then Some new_count
+            if new_count > 0 then Some new_count
+            else if new_count = 0 then None
             else raise (Insufficient v)
         | None -> raise (Insufficient 0))
       !b
