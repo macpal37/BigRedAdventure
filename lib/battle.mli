8d1bcc552e645b1ef5cbc047001ca6f784592dfd
Alan Chen <aac77@cornell.edu>
2022-03-14 17:50:45 -0400

    Outlined game engine framework

--- lib/battle.mli
+++ lib/battle.mli
@@ -2,11 +2,6 @@
 
     This module orchestrates the gameplay in battle*)
 
-val run_tick : char -> bool
+val run_tick : char option -> unit
 (** [run_tick c] runs a new tick of the battle. [c] represents the last
-    character that the user is pressing. The return value indicates
-    whether the battle has finished.*)
-
-val draw : unit -> unit
-(** [draw _] redraws the scene on the window. It may be called from
-    other modules if the window has been forcibly refreshed.*)
+    character that the user is pressing.*)
--- lib/controller.mli
+++ lib/controller.mli
@@ -2,3 +2,12 @@
 
     This module primarily takes care of user inputs and forwarding them
     to the appropriate game module*)
+
+type modes =
+  | ModeOverworld
+  | ModeBattle
+  | ModeMenu
+      (** The modes the game may be in; specifies which module is
+          handling ticks*)
+
+val main : unit -> unit
