661dabf4ccd0096bd4a044011e0914843d29aaf1
Alan Chen <aac77@cornell.edu>
2022-03-14 11:06:25 -0400

    Updated test main to run more tests

--- test/main.ml
+++ test/main.ml
@@ -1,6 +1,8 @@
 open OUnit2
 
 let suite =
-  "test suite for Final Project" >::: List.flatten [ Itemtest.tests ]
+  "test suite for Final Project"
+  >::: List.flatten
+         [ Itemtest.tests; Inventorytest.tests; Playertest.tests ]
 
 let _ = run_test_tt_main suite
