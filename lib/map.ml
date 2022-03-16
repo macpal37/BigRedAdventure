dc0890b081e88f5906d846e8158b2ef33a542d90
CollinWoo <collinwu01@gmail.com>
2022-03-16 01:20:54 -0400

    reformat map.ml and map.mli

--- lib/map.ml
+++ lib/map.ml
@@ -28,49 +28,56 @@ type t = tile array array
 let json_val json f key = json |> member key |> f
 let json_list json f key = json |> member key |> to_list |> List.map f
 
-let string_to_range (s:string) = 
-  match (String.split_on_char '-' s) |> List.map int_of_string with
-  | [a; b] -> (a, b)
-  | _ | exception Failure _ -> raise (Malformed_Json "Invalid level range")
-   
+let string_to_range (s : string) =
+  match String.split_on_char '-' s |> List.map int_of_string with
+  | [ a; b ] -> (a, b)
+  | _ | (exception Failure _) ->
+      raise (Malformed_Json "Invalid level range")
+
 let encounter_of_json json =
   {
     name = json_val json to_string "creatures";
     rate = json_val json to_float "encounter_rate";
-    levels = json_val json to_string "level_range" |> string_to_range
+    levels = json_val json to_string "level_range" |> string_to_range;
   }
 
-let encounters_of_json json = (json |> to_list |> List.map encounter_of_json)
+let encounters_of_json json =
+  json |> to_list |> List.map encounter_of_json
 
-let all_encounters_of_json json = json_list json encounters_of_json "encounters" |> Array.of_list
+let all_encounters_of_json json =
+  json_list json encounters_of_json "encounters" |> Array.of_list
 
-let sprites_of_json json = json_list json to_string "tile_sprites" |> Array.of_list
+let sprites_of_json json =
+  json_list json to_string "tile_sprites" |> Array.of_list
 
-let list_to_tile_type encounters (s:string) =
-  let type_code = String.get s 0 in 
-  if type_code = 'p' then Path 
-  else if type_code = 'o' then Obstacle 
-  else Grass (String.sub s 1 (String.length s - 1) |> int_of_string |> Array.get encounters)
+let list_to_tile_type encounters (s : string) =
+  let type_code = String.get s 0 in
+  if type_code = 'p' then Path
+  else if type_code = 'o' then Obstacle
+  else
+    Grass
+      (String.sub s 1 (String.length s - 1)
+      |> int_of_string |> Array.get encounters)
 
-let code_to_tile graphics encounters (s:string) = 
+let code_to_tile graphics encounters (s : string) =
   match String.split_on_char '|' s with
-  | [h; t] -> {
-    graphic = int_of_string t |> Array.get graphics;
-    ttype = list_to_tile_type encounters h
-  }
+  | [ h; t ] ->
+      {
+        graphic = int_of_string t |> Array.get graphics;
+        ttype = list_to_tile_type encounters h;
+      }
   | _ -> raise (Malformed_Json "Invalid map tile")
 
 let ll_to_matrix ll = Array.of_list (List.map Array.of_list ll)
-
 let matrix_map f m = Array.(map (map f) m)
+let codes_of_json json = json |> to_string |> String.split_on_char ','
 
-let codes_of_json json = 
-  json |> to_string |> String.split_on_char ','
-
-let init_map json = 
+let init_map json =
   let e = all_encounters_of_json json in
   let g = sprites_of_json json in
-  json |> member "map" |> to_list |> List.map codes_of_json |> ll_to_matrix |> matrix_map (code_to_tile g e)
+  json |> member "map" |> to_list |> List.map codes_of_json
+  |> ll_to_matrix
+  |> matrix_map (code_to_tile g e)
 
 let get_dim map = (Array.length map, Array.get map 0 |> Array.length)
 
@@ -90,17 +97,22 @@ let get_graphic_id t c =
   match get_tile t c with
   | { graphic; _ } -> graphic
 
+let creature_level (min, max) = min + Random.int (max - min + 1)
 
-let creature_level (min, max) =
-  min + Random.int (max - min + 1)
+type random_pokemon = {
+  name : string;
+  level : int;
+}
 
-type random_pokemon = {name:string; level:int}
-let rec creature_type (e:encounters) (v:float) = 
+let rec creature_type (e : encounters) (v : float) =
   match e with
   | [] -> None
-  | h::t -> if (h.rate >= v) then Some {name = h.name; level = creature_level h.levels} else creature_type t v
+  | h :: t ->
+      if h.rate >= v then
+        Some { name = h.name; level = creature_level h.levels }
+      else creature_type t v
 
-let encounter_creature e = 
+let encounter_creature e =
   match creature_type e (Random.float 1.0) with
   | None -> None
-  | Some {name; level} -> Some (Creature.create_creature name level)e
+  | Some { name; level } -> Some (Creature.create_creature name level)
--- lib/map.mli
+++ lib/map.mli
@@ -16,11 +16,11 @@ type encounters
 type tile_type =
   | Path
   | Grass of encounters
-  | Obstacle 
-(** type representing the type of a tile on the map *)
+  | Obstacle  (** type representing the type of a tile on the map *)
 
 val init_map : Yojson.Basic.t -> t
-(** [init_map j] is the map represented by j. Raises [Malformed_Json] if the json file is invalid *)
+(** [init_map j] is the map represented by j. Raises [Malformed_Json] if
+    the json file is invalid *)
 
 val get_dim : t -> int * int
 (** [get_dim m] is a tuple [(width, height)] representing the number of
@@ -28,7 +28,8 @@ val get_dim : t -> int * int
 
 val get_type : t -> coord -> tile_type
 (** [get_type m c] is the type ([Path, Grass, Obstacle]) of the tile at
-    [c] in map [m]. Raises [Out_of_Bounds] if [c] is not a valid coordinate *)
+    [c] in map [m]. Raises [Out_of_Bounds] if [c] is not a valid
+    coordinate *)
 
 val get_graphic_id : t -> coord -> string
 (** [get_graphic_id m c] is the id of the graphic of the tile at [c] in
@@ -36,4 +37,5 @@ val get_graphic_id : t -> coord -> string
 
 val encounter_creature : encounters -> Creature.creature option
 (** [encounter_creature e] is the random creature that is encountered
-    based on the specification in [e]. Returns None if no creature is present *)
+    based on the specification in [e]. Returns None if no creature is
+    encountered *)
