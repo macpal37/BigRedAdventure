91f3714ece3664891d6e583b40843483fd789b9e
“Marco <aquanautx37@gmail.com>
2022-03-21 12:23:56 -0400

    Working on Item Menu

--- lib/Other/util.ml
+++ lib/Other/util.ml
@@ -4,3 +4,7 @@ let bound num min max =
 let rand max () =
   Random.self_init ();
   Random.int max
+
+let captilize_all_string str =
+  let lst = String.split_on_char ' ' str in
+  List.fold_left (fun x y -> x ^ " " ^ String.capitalize_ascii y) "" lst
