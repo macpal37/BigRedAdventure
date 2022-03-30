ed5a97cef7ea4927c1c1f596ce644cece23eea86
Alan Chen <aac77@cornell.edu>
2022-03-30 12:02:22 -0400

    Added smooth walking

--- lib/Controllers/overworld.ml
+++ lib/Controllers/overworld.ml
@@ -7,11 +7,48 @@ let player_spritesheet =
     "assets/gui_sprites/player_sprite_overworld.png" 64 64 1
 
 let player_sprite_n = Spritesheet.get_sprite player_spritesheet 6
+
+let player_sprite_n_walk =
+  Util.list_index_fun
+    [
+      Spritesheet.get_sprite player_spritesheet 7;
+      Spritesheet.get_sprite player_spritesheet 8;
+      Spritesheet.get_sprite player_spritesheet 6;
+    ]
+
 let player_sprite_s = Spritesheet.get_sprite player_spritesheet 0
+
+let player_sprite_s_walk =
+  Util.list_index_fun
+    [
+      Spritesheet.get_sprite player_spritesheet 1;
+      Spritesheet.get_sprite player_spritesheet 2;
+      Spritesheet.get_sprite player_spritesheet 0;
+    ]
+
 let player_sprite_e = Spritesheet.get_sprite player_spritesheet 11
+
+let player_sprite_e_walk =
+  Util.list_index_fun
+    [
+      Spritesheet.get_sprite player_spritesheet 10;
+      Spritesheet.get_sprite player_spritesheet 9;
+      Spritesheet.get_sprite player_spritesheet 11;
+    ]
+
 let player_sprite_w = Spritesheet.get_sprite player_spritesheet 3
 
-let draw_tiles offset_x offset_y orie =
+let player_sprite_w_walk =
+  Util.list_index_fun
+    [
+      Spritesheet.get_sprite player_spritesheet 4;
+      Spritesheet.get_sprite player_spritesheet 5;
+      Spritesheet.get_sprite player_spritesheet 3;
+    ]
+
+let draw_tiles offset_x offset_y =
+  Graphics.set_color Graphics.black;
+  Graphics.fill_rect 0 0 800 720;
   for i = 0 to Map.get_width (State.map ()) - 1 do
     for j = 0 to Map.get_height (State.map ()) - 1 do
       let s = Map.get_sprite (State.map ()) (i, j) in
@@ -20,18 +57,17 @@ let draw_tiles offset_x offset_y orie =
         ((j * tile_size) - offset_y)
         ()
     done
-  done;
-  match orie with
-  | Player.N -> Draw.draw_sprite player_sprite_n 368 328 ()
-  | Player.E -> Draw.draw_sprite player_sprite_e 368 328 ()
-  | Player.S -> Draw.draw_sprite player_sprite_s 368 328 ()
-  | Player.W -> Draw.draw_sprite player_sprite_w 368 328 ()
+  done
 
 let draw _ =
   draw_tiles
     ((Player.x (State.player ()) * tile_size) - ((800 - tile_size) / 2))
-    ((Player.y (State.player ()) * tile_size) - ((720 - tile_size) / 2))
-    (Player.orie (State.player ()))
+    ((Player.y (State.player ()) * tile_size) - ((720 - tile_size) / 2));
+  match Player.orie (State.player ()) with
+  | Player.N -> Draw.draw_sprite player_sprite_n 368 328 ()
+  | Player.E -> Draw.draw_sprite player_sprite_e 368 328 ()
+  | Player.S -> Draw.draw_sprite player_sprite_s 368 328 ()
+  | Player.W -> Draw.draw_sprite player_sprite_w 368 328 ()
 
 let encounter_anim _ =
   for i = 1 to 60 do
@@ -44,14 +80,41 @@ let encounter_anim _ =
     | Player.S -> Draw.draw_sprite player_sprite_s 368 328 ()
     | Player.W -> Draw.draw_sprite player_sprite_w 368 328 ());
     Graphics.auto_synchronize true;
-    Unix.sleepf 0.016
+    Input.sleep 0.016 ()
   done;
-  Unix.sleepf 0.1
+  Input.sleep 0.1 ()
+
+let move_scroll dx dy =
+  for i = 1 to 14 do
+    Graphics.auto_synchronize false;
+    draw_tiles
+      ((Player.x (State.player ()) * tile_size)
+      - ((800 - tile_size) / 2)
+      + (i * dx * tile_size / 15))
+      ((Player.y (State.player ()) * tile_size)
+      - ((720 - tile_size) / 2)
+      + (i * dy * tile_size / 15));
+    (match Player.orie (State.player ()) with
+    | Player.N ->
+        Draw.draw_sprite (player_sprite_n_walk (i / 5 mod 3)) 368 328 ()
+    | Player.E ->
+        Draw.draw_sprite (player_sprite_e_walk (i / 5 mod 3)) 368 328 ()
+    | Player.S ->
+        Draw.draw_sprite (player_sprite_s_walk (i / 5 mod 3)) 368 328 ()
+    | Player.W ->
+        Draw.draw_sprite (player_sprite_w_walk (i / 5 mod 3)) 368 328 ());
+    Graphics.auto_synchronize true;
+    Input.sleep 0.016 ()
+  done
 
 let attempt_move dx dy orie =
+  Player.set_orie orie (State.player ());
+
   let new_x, new_y = (State.player_x () + dx, State.player_y () + dy) in
-  (match Map.get_type (State.map ()) (new_x, new_y) with
-  | Path -> Player.set_coord new_x new_y (State.player ())
+  match Map.get_type (State.map ()) (new_x, new_y) with
+  | Path ->
+      move_scroll dx dy;
+      Player.set_coord new_x new_y (State.player ())
   | Obstacle -> ()
   | Grass e ->
       if Random.float 1. < 0.2 then (
@@ -60,9 +123,8 @@ let attempt_move dx dy orie =
         match c with
         | Some c -> Battle.start_battle c
         | None -> failwith "no creature encountered")
-      else Player.set_coord new_x new_y (State.player ()));
-  Player.set_orie orie (State.player ())
-(*let move_scroll _ = () *)
+      else move_scroll dx dy;
+      Player.set_coord new_x new_y (State.player ())
 
 let ui_refresh _ =
   Ui.clear_ui Ui.Background;
