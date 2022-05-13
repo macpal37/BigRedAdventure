3132fad609a6a85ae3b75f75719a3c0bae2f35aa
Alan Chen <aac77@cornell.edu>
2022-05-13 17:26:36 -0400

    Added alpha testing

--- bin/main.ml
+++ bin/main.ml
@@ -10,15 +10,21 @@ let rec loop w r t =
   | Some (KeyUp e) ->
       print_endline ("Key Released: " ^ Sdlkeycode.to_string e.keycode)
   | _ -> ());
-  Sdlrender.set_draw_color r
-    ~rgb:(Random.int 256, Random.int 256, Random.int 256)
-    ~a:255;
-  Sdlrender.fill_rect r
-    (Sdlrect.make1
-       (Random.int 50, Random.int 50, Random.int 150, Random.int 150));
+  for _ = 0 to 10 do
+    Sdlrender.set_draw_color r
+      ~rgb:(Random.int 256, Random.int 256, Random.int 256)
+      ~a:255;
+    Sdlrender.fill_rect r
+      (Sdlrect.make1
+         (Random.int 50, Random.int 50, Random.int 150, Random.int 150))
+  done;
+  Sdlrender.set_draw_color r ~rgb:(0, 0, 0) ~a:100;
+  Sdlrender.fill_rect r (Sdlrect.make1 (0, 0, 100, 100));
   Sdlrender.render_present r;
-  print_endline (string_of_float (Sys.time () -. t));
-  loop w r (Sys.time ())
+  Sdlrender.clear r;
+  Unix.sleepf 0.016;
+  print_endline (string_of_float (Unix.gettimeofday () -. t));
+  loop w r (Unix.gettimeofday ())
 
 let main _ =
   Random.init 69;
@@ -30,8 +36,9 @@ let main _ =
   in
   let renderer =
     Sdlrender.create_renderer ~win:window ~index:(-1)
-      ~flags:[ PresentVSync ]
+      ~flags:[ PresentVSync; Accelerated ]
   in
+  Sdlrender.set_draw_blend_mode renderer Blend;
   Sdlrender.set_viewport renderer (Sdlrect.make1 (100, 100, 300, 300));
 
   Sdlrender.set_viewport renderer (Sdlrect.make1 (50, 50, 300, 300));
